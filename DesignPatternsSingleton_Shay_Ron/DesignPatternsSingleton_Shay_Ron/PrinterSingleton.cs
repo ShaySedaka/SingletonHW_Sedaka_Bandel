bdcfa724a0d105c14691296c118a32ff25ebc2e3
ShaySedaka <shaysedaka@gmail.com>
2021-11-14 17:32:22 +0200

    Initial Commit

---
+++ DesignPatternsSingleton_Shay_Ron/DesignPatternsSingleton_Shay_Ron/PrinterSingleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsSingleton_Shay_Ron
+{
+    public sealed class PrinterSingleton
+    {
+        private static PrinterSingleton _instance;
+        private static object _lockThis = new object();
+
+        public Queue<string> _printerJobs;
+
+        private PrinterSingleton() {
+            _printerJobs = new Queue<string>();
+        }
+
+        public static PrinterSingleton GetSingleton()
+        {
+            lock (_lockThis)
+            {
+                if (_instance == null) _instance = new PrinterSingleton();
+            }
+            return _instance;
+        }
+
+        public  void AddPrinterJob(string job)
+        {
+            GetSingleton()._printerJobs.Enqueue(job);
+        }
+        public  string PrintFromJobQueue()
+        {
+            if (GetSingleton()._printerJobs.Count > 0)
+            { 
+                string job = GetSingleton()._printerJobs.Dequeue();
+                Console.WriteLine(job);
+
+                return job;
+            }
+
+            return "This printer has an empty job queue.";
+
+        }
+    }
+}
---
+++ DesignPatternsSingleton_Shay_Ron/DesignPatternsSingleton_Shay_Ron/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsSingleton_Shay_Ron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== A NEW DAY IN THE BORING OFFICE ===\n\n");
+            Console.WriteLine("- There is one very old industrial printer in the office, connected to the network.\n-------------------------------------------------\n");
+
+            Console.WriteLine("Bob opens up his WORD client and enqueues 2 jobs for the printer...\n\n");
+
+            PrinterSingleton bobsWordClient = PrinterSingleton.GetSingleton();
+
+            bobsWordClient.AddPrinterJob("Hello, I am the first job from Bob's computer.");
+            bobsWordClient.AddPrinterJob("Hello, I am the second job from Bob's computer.");
+
+
+            Console.WriteLine("Mike opens up his WORD client and enqueues 1 job for the printer...\n\n");
+
+            PrinterSingleton mikesWordClient = PrinterSingleton.GetSingleton();
+
+            mikesWordClient.AddPrinterJob("Hello, I am the first job from Mike's computer.");
+
+
+            Console.WriteLine("- Mike presses the PRINT button 3 times and this comes out...\n\n");
+
+            mikesWordClient.PrintFromJobQueue();
+            mikesWordClient.PrintFromJobQueue();
+            mikesWordClient.PrintFromJobQueue();
+
+            Console.ReadKey();
+
+        }
+    }
+}
